637b73a6169a223a1c9f8bb34c659ccfa92a46ba
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2017-02-17 00:47:07 +0700

    fix

--- scripts/install.js
+++ scripts/install.js
@@ -2,6 +2,5 @@ var fs = require('fs');
 var path = require('path');
 
 if (process.platform === 'linux') {
-    fs.chmodSync(path.join(__dirname, '..', 'bin', 'winexe_centos_x64'), 777);
-    fs.chmodSync(path.join(__dirname, '..', 'bin', 'winexe_ubuntu_x64'), 777);
+    fs.chmodSync(path.join(__dirname, '..', 'bin', 'winexe_x64'), 777);
 }
